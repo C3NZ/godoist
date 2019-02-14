2257c0adb771bba3c602251ea674792aa9b90b26
C3NZ <cenz@cenz.io>
2019-02-14 15:44:37 -0800

    [feat] properly parse the input coming into the do command to for completing tasks

--- cmd/add.go
+++ cmd/add.go
@@ -2,15 +2,18 @@ package cmd
 
 import "github.com/spf13/cobra"
 import "fmt"
+import "strings"
 
 var addCmd = &cobra.Command{
     Use: "add",
     Short: "Add a task to the to do list",
     Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("Add called")  
+        task := strings.Join(args, " ")
+        fmt.Printf("Added \"%s\" to your task list \n", task)
     },
 }
 
+// Go initializer function called to setup anything before execution
 func init() {
     // Register the addCmD as a sub command
     RootCmd.AddCommand(addCmd)
