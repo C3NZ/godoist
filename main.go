94cb0e1dd5d01713c9ddbe58d5d91d0032c6a487
C3NZ <cenz@cenz.io>
2019-02-14 17:25:22 -0800

    [feat] import homedir to get the users home dir and then initialize the databse with our new setup

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "godoist/cmd"
+// We import go-homedir as homedir
+import (
+    "path/filepath"
+    "godoist/cmd"
+    "godoist/db"
+
+    homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
+    // Obtain the homedir path for the user currently running this program
+    home, _ := homedir.Dir()
+
+    // Join the home directory with our desired file path
+    dbPath := filepath.Join(home, "tasks.db")
+    
+    // Instantiate our db connection/buckets
+    err:= db.Init(dbPath)
+
+    // Crash if setting up our database fails
+    if err != nil {
+        panic(err)
+    }
     // Execute the root command (Describe our app)
     cmd.RootCmd.Execute();
 }
